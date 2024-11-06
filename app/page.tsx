416db7eaf84a5c20d3b79b6e5dc9de2a521ea7d9
markbrn1a <httpx.eu@gmail.com>
2024-11-06 18:36:57 +0100

    tech icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Clients from "./components/clients";
 import Hero from "./components/UI/Hero";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
+      <Clients />
     </main>
   );
 }
