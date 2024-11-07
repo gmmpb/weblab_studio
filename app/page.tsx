33f8d9b10e1f058450af72341a029d323e32e6d4
markbrn1a <httpx.eu@gmail.com>
2024-11-07 16:12:45 +0100

    self undefined

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@
 import Clients from "./components/clients";
 import CustomersList from "./munkaink/customers-list";
 import Hero from "./components/UI/Hero";
-import FaceMesh from "./components/faceMesh";
+import dynamic from "next/dynamic";
+
+const FaceMesh = dynamic(() => import("./components/faceMesh"), {
+  ssr: false,
+});
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
